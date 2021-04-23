fd2b956ab7965b0b27194a233696e139e4bad62e
Rishi <rishi.sek@gmail.com>
2021-04-23 03:49:03 -0500

    Add enemy management to Ikaruga world

--- include/ikaruga/core/world/world.h
+++ include/ikaruga/core/world/world.h
@@ -3,19 +3,25 @@
 //
 #pragma once
 
-#include <ikaruga/core/objects/projectile/projectile.h>
 #include <game_engine/game_world.h>
+#include <ikaruga/core/objects/projectile/projectile.h>
+#include <ikaruga/core/objects/enemy/enemy.h>
 
 namespace ikaruga::world {
-using objects::projectile::Projectile;
 class World : public game_engine::GameWorld {
  public:
   World();
-  const std::vector<Projectile *> &GetProjectiles() const;
+  const std::vector<objects::projectile::Projectile *> &GetProjectiles() const;
+  const std::vector<std::unique_ptr<ikaruga::objects::enemy::Enemy>> &GetEnemies() const;
   void Update() override;
-  void AddProjectile(Projectile *const projectile);
+  void AddProjectile(objects::projectile::Projectile *const projectile);
+  void World::AddEnemy(std::unique_ptr<ikaruga::objects::enemy::Enemy> &&enemy);
+  virtual ~World();
  private:
+  std::vector<objects::projectile::Projectile *> projectiles_;
+  std::vector<std::unique_ptr<ikaruga::objects::enemy::Enemy>> enemies_;
+
   void UpdateProjectiles();
-  std::vector<Projectile *> projectiles_;
+  void UpdateEnemies();
 };
 }
--- src/ikaruga/core/world/world.cc
+++ src/ikaruga/core/world/world.cc
@@ -2,9 +2,13 @@
 // Created by rishi on 16-04-2021.
 //
 
-#include "ikaruga/core/world/world.h"
+#include <ikaruga/core/world/world.h>
+#include <game_engine/components/physics_component.h>
 
 namespace ikaruga::world {
+using objects::projectile::Projectile;
+using ikaruga::objects::enemy::Enemy;
+
 const std::vector<Projectile *> &World::GetProjectiles() const {
   return projectiles_;
 }
@@ -15,13 +19,39 @@ void World::UpdateProjectiles() {
   }
 }
 
+void World::UpdateEnemies() {
+  for (std::unique_ptr<Enemy> &enemy: enemies_) {
+    enemy->Update(*this);
+  }
+  for (Projectile *projectile: projectiles_) {
+    enemies_.erase(std::remove_if(enemies_.begin(),
+                                  enemies_.end(),
+                                  [&](std::unique_ptr<Enemy> &obj) {
+                                    return obj->GetPhysicsComponent()->Collides(
+                                        projectile);
+                                  }),
+                   enemies_.end());
+  }
+}
+
 void World::Update() {
   UpdateProjectiles();
+  UpdateEnemies();
 }
 
 void World::AddProjectile(Projectile *const projectile) {
   projectiles_.push_back(projectile);
 }
 
+void World::AddEnemy(std::unique_ptr<Enemy> &&enemy) {
+  enemies_.push_back(std::move(enemy));
+}
+
 World::World() {}
+
+World::~World() {}
+
+const std::vector<std::unique_ptr<Enemy>> &World::GetEnemies() const {
+  return enemies_;
+}
 }
